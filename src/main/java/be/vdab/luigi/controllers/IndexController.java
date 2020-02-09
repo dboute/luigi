fc6f7db46d099d537df2b4aba2f92a4893a46777
Dennis Boute <dennis.boute@gmail.com>
2020-02-09 13:43:51 +0100

    Controller

---
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -0,0 +1,28 @@
+package be.vdab.luigi.controllers;
+
+import java.time.LocalTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+class IndexController {
+    @GetMapping
+    public String index() {
+        StringBuffer buffer =
+                new StringBuffer("<!doctype html><html><title>Hallo</title><body>");
+        int uur = LocalTime.now().getHour();
+
+        if (uur < 12) {
+            buffer.append("Goede morgen");
+        } else if (uur < 18) {
+            buffer.append("Goede middag");
+        } else {
+            buffer.append("Goede avond");
+        }
+        buffer.append("</body></html>");
+        return buffer.toString();
+    }
+} 
