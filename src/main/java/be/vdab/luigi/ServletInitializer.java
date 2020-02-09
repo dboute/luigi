f821834b27eca3c841132040fe97e2163a855dcd
Dennis Boute <dennis.boute@gmail.com>
2020-02-09 13:16:14 +0100

    Initial commit

---
+++ src/main/java/be/vdab/luigi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package be.vdab.luigi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(LuigiApplication.class);
+    }
+
+}
